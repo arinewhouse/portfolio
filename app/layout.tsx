6fc1707dead7405f4ed95fc4576e371a9fb4b396
arinewhouse <114793334+arinewhouse@users.noreply.github.com>
2023-05-15 06:00:23 -0400

    edit

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,9 +36,9 @@ export const metadata: Metadata = {
 			"max-image-preview": "large",
 			"max-snippet": -1,
 		},
-		icons: {
-			shortcut: "/favicon.png",
-		},
+	icons: {
+		shortcut: "/favicon.png",
+	},
 	},
 };
 const inter = Inter({
